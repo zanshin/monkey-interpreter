f67a1c2d916f4ba217f300e4594a0eb4a6fe42d0
Mark Nichols <mark@zanshin.net>
2021-08-07 08:50:02 -0500

    Created lexer test

---
+++ lexer/lexer_test.go
@@ -0,0 +1,40 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/zanshin/interpreter/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected %q, got %q", i, tt.expetedType, tok.Type)
+		}
+
+		if tok.literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - litearl wrong. expected %q, got %q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
